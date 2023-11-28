2ecf083486e5b61725bd7a8e72e0105f48fa575c
Rajneesh-pine <145674922+Rajneesh-pine@users.noreply.github.com>
2023-11-28 13:43:42 +0530

    Create bank_man.h

---
+++ bank_man.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+typedef struct user {
+    char name[20];
+    char dob[11];
+    long long phoneno;
+    long long aadharno;
+    char account[7];
+    int balance;
+} u;
